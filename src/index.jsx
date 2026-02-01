ac7ee5619c62d9131ed63137482161925b776b33
Matthew Oldfield <moldfield95@googlemail.com>
2026-02-01 12:52:12 +0000

    Port to Vite

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import App from './Core/App';
 import reportWebVitals from './Core/reportWebVitals';
 import theme from './Core/Theme'
 
-
 ReactDOM.render(
   <React.StrictMode>
       <ThemeProvider theme={theme}>
@@ -16,7 +15,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
