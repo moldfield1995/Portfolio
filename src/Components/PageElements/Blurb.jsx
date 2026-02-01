d9229b123d4e045deb05e4e17d33fde28253d3ae
Matthew Oldfield <moldfield95@googlemail.com>
2026-02-01 20:19:58 +0000

    Update Blurb

--- src/Components/PageElements/Blurb.jsx
+++ src/Components/PageElements/Blurb.jsx
@@ -5,8 +5,8 @@ import { Container,  Typography } from '@material-ui/core';
 export default function Blurb() {
     return <Container maxWidth="sm">
         <Typography variant="body2" gutterBottom>
-            I am a web developer with a games programming background. <br/>
-            I have developed a variety of websites from: In house quoting tools, custom e-commerce solutions, company pages with CMS backend, social media schedulers, and in house sales analysis web tools. <br/>
+            A long-time tech enthusiast turned Software Developer, I transitioned from game development to full-stack web development. Currently, I focus on optimizing applications and building innovative healthcare solutions. <br/>
+            My background spans custom websites, eCommerce, and the games industry, including a published title, Flux8. I am now dedicated to leveraging my skills to solve unique challenges and develop impactful products.
             Majority of the sites were written with a .Net Core API using monolithic API architecture and React.js front end, with others built with .Net MVC using SASS and js.
         </Typography>
     </Container>
